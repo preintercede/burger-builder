8c9023607dccb791c490ed2700eddc9410e498f1
Dylan <dylanw@uci.edu>
2020-08-11 15:47:24 -0700

    inital commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Auxx from "../../hoc/Auxx";
+
+const layout = (props) => (
+  <Auxx>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main>{props.children}</main>
+  </Auxx>
+);
+
+export default layout;
